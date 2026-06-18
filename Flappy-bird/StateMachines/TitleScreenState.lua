1d2edfa73a51e8fb1abbf9cc0440e2a449716143
Maria Striletska <mstrel2019@gmail.com>
2026-06-18 16:43:00 +0300

    [flappy-bird] update title screen texts

--- Flappy-bird/StateMachines/TitleScreenState.lua
+++ Flappy-bird/StateMachines/TitleScreenState.lua
@@ -8,7 +8,37 @@ function TitleScreenState:update(dt)
 end
 
 function TitleScreenState:render()
-    love.graphics.newFont('font.ttf', 20)
-    love.graphics.setColor(0, 0, 0)
-    love.graphics.printf('Flappy Bird', 0, 100, const.VIRTUAL_WIDTH, 'center')
+    local font = 'assets/font.ttf'
+    local largeFont = love.graphics.newFont(font, 40)
+    love.graphics.setFont(largeFont)
+    local nameText = "Flappy Bird"
+    local nameTextWidth = largeFont:getWidth(nameText)
+    local textX = const.VIRTUAL_WIDTH/2 - nameTextWidth/2
+    local textY = const.VIRTUAL_HEIGHT/2
+    love.graphics.setColor(0, 0, 0, 0.5)
+    love.graphics.print(nameText, const.VIRTUAL_WIDTH/2 - nameTextWidth/2 + 3, const.VIRTUAL_HEIGHT/4 + 3)
+    love.graphics.setColor(1, 0.84, 0)
+    love.graphics.print(nameText, const.VIRTUAL_WIDTH/2 - nameTextWidth/2, const.VIRTUAL_HEIGHT/4)
+
+    local middleFont = love.graphics.newFont(font, 30)
+    love.graphics.setFont(middleFont)
+    local nameText = "Press 'Enter' to start"
+    local nameTextWidth = middleFont:getWidth(nameText)
+    local textX = const.VIRTUAL_WIDTH/2 - nameTextWidth/2
+    local textY = const.VIRTUAL_HEIGHT/2
+    love.graphics.setColor(0, 0, 0, 0.5)
+    love.graphics.print(nameText, const.VIRTUAL_WIDTH/2 - nameTextWidth/2 + 3, const.VIRTUAL_HEIGHT/2 + 3)
+    love.graphics.setColor(1, 0.65, 0)
+    love.graphics.print(nameText, const.VIRTUAL_WIDTH/2 - nameTextWidth/2, const.VIRTUAL_HEIGHT/2)
+
+    local smallFont = love.graphics.newFont(font, 20)
+    love.graphics.setFont(smallFont)
+    local nameText = "By Maria Striletska"
+    local nameTextWidth = smallFont:getWidth(nameText)
+    local textX = const.VIRTUAL_WIDTH/2 - nameTextWidth/2
+    local textY = const.VIRTUAL_HEIGHT/2
+    love.graphics.setColor(0, 0, 0, 0.5)
+    love.graphics.print(nameText, const.VIRTUAL_WIDTH/2 - nameTextWidth/2 + 3, const.VIRTUAL_HEIGHT/1.5 + 3)
+    love.graphics.setColor(1, 0.65, 0)
+    love.graphics.print(nameText, const.VIRTUAL_WIDTH/2 - nameTextWidth/2, const.VIRTUAL_HEIGHT/1.5) 
 end
